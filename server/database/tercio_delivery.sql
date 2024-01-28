3f00db3a7ec01503fd4db655411213518dc86da8
José Roberto <jrobertogram@gmail.com>
2024-01-27 21:34:14 -0300

    subindo o back end

---
+++ server/database/tercio_delivery.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 28/01/2024 às 01:11
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tercio_delivery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `cliente_id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `saldo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `clientes`
+--
+
+INSERT INTO `clientes` (`cliente_id`, `nome`, `senha`, `tipo`, `saldo`) VALUES
+(1, 'Noemy Roberta', '1234', 'cliente', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `lanche`
+--
+
+CREATE TABLE `lanche` (
+  `lanche_id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `valor` double NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `quantDispo` int(3) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `pedido_id` int(11) NOT NULL,
+  `cliente_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedido_lanches`
+--
+
+CREATE TABLE `pedido_lanches` (
+  `pedido_lanches_id` int(11) NOT NULL,
+  `pedido_id` int(11) NOT NULL,
+  `lanche_id` int(11) NOT NULL,
+  `quantDispo` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`cliente_id`);
+
+--
+-- Índices de tabela `lanche`
+--
+ALTER TABLE `lanche`
+  ADD PRIMARY KEY (`lanche_id`);
+
+--
+-- Índices de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`pedido_id`),
+  ADD UNIQUE KEY `cliente_id` (`cliente_id`);
+
+--
+-- Índices de tabela `pedido_lanches`
+--
+ALTER TABLE `pedido_lanches`
+  ADD PRIMARY KEY (`pedido_lanches_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `lanche`
+--
+ALTER TABLE `lanche`
+  MODIFY `lanche_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `pedido_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedido_lanches`
+--
+ALTER TABLE `pedido_lanches`
+  MODIFY `pedido_lanches_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server/index.php
@@ -0,0 +1,453 @@
+<?php
+
+setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+date_default_timezone_set('America/Sao_Paulo');
+
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: X-Requested-With,content-type');
+header('Access-Control-Allow-Credentials: true');
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Pragma: no-cache");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:  {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+
+define('SECRET_KEY','878JHdD89yd4DUdb7ysfN63dnYDbofs');
+define('ALGORITHM','HS256');
+
+require 'vendor/autoload.php';
+
+use Medoo\Medoo;
+
+$database = new Medoo([
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'tercio_delivery',
+    'username' => 'root',
+    'password' => '',
+    'error' => PDO::ERRMODE_SILENT,
+]);
+
+
+$url = isset($_GET['url']) ? $_GET['url'] : null;
+if($url === null){
+    echo json_encode(['ok' => false, 'message' => 'Método não existe']);
+    exit();
+}
+
+$parametros = explode('/', $url);
+if (empty($parametros)) {
+    echo json_encode(['ok' => false, 'message' => 'Método não existe']);
+    exit();
+}
+
+$method = $parametros[0];
+
+$permissoes = [
+    "comprador" => ['DepositarMoney', 'GetMoney', 'RetirarMoney', 'GetLanche', 'CreatePrefeitura','GetLanches', 'FazerPedido'],
+    "Vendedor" => ['CreateLanche','UpdateLanche', 'DeleteLanche', 'DeletePrefeitura', 'GetPedidos'],
+];
+
+define('CLIENTE_ID', 1);
+define('VENDEDOR_ID', 1);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    setRota($method);
+}
+
+function setRota($funcao){
+
+    header('Context-Type: application/json; charset=utf8');
+
+    if (!empty($_POST)) {
+        $data = json_encode($funcao($_POST));
+    } elseif (!empty(file_get_contents('php://input'))) {
+		$data = json_encode($funcao(json_decode(file_get_contents('php://input'), true)));
+    } else {
+        $data = json_encode($funcao());
+    }
+
+    echo $data;
+
+}
+
+//$rt = DepositarMoney(["cliente_id" => 1, "valor" => 10]);
+//print_r($rt);
+
+function DepositarMoney($dataf){
+    global  $database;
+
+    $validacoes = [
+        "valor" => ["type" => "numeric", "required" => false],
+    ];
+
+    $validar = validarDados($validacoes, $dataf);
+
+    if($validar['ok'] === true){
+        $data = $validar['dados'];
+    }else{
+        return $validar;
+    }
+
+    $saldo = $database->get("clientes", "saldo", ['cliente_id' => CLIENTE_ID]);
+
+    $novo_valor = $saldo + $data['valor'];
+
+    $cliente = $database->update("clientes", ["saldo" => $novo_valor], ["cliente_id" => CLIENTE_ID]);
+
+    if($database->errorInfo){
+        error_log(implode(', ', $database->errorInfo));
+        return ['ok' => false, "message" => $database->errorInfo[2], 'code' => $database->errorInfo[1]];
+    }
+
+    if(empty($cliente->rowCount())){
+        return ['ok' => false, "message" => 'Nenhuma alteração foi efetuada.', 'code' => -1];
+    }
+
+    return ['ok' => true, 'saldo' => $novo_valor];
+
+}
+
+//$rt = GetMoney();
+//print_r($rt);
+
+function GetMoney(){
+
+    global  $database;
+
+    $saldo = $database->get("clientes", "saldo", ['cliente_id' => CLIENTE_ID]);
+
+    if($database->errorInfo){
+        error_log(implode(', ', $database->errorInfo));
+        return ['ok' => false, "message" => $database->errorInfo[2], 'code' => $database->errorInfo[1]];
+    }
+
+    if(empty($saldo)){
+        return ['ok' => false, "message" => 'Nenhuma dado foi retornado', 'code' => -1];
+    }
+
+    return ['ok' => true, 'saldo' => $saldo];
+
+}
+
+//$rt = RetirarMoney(["valor" => 5]);
+//print_r($rt);
+
+function RetirarMoney($dataf){
+    global  $database;
+
+    $validacoes = [
+        "valor" => ["type" => "numeric", "required" => false],
+    ];
+
+    $validar = validarDados($validacoes, $dataf);
+
+    if($validar['ok'] === true){
+        $data = $validar['dados'];
+    }else{
+        return $validar;
+    }
+
+    $saldo = $database->get("clientes", "saldo", ['cliente_id' => CLIENTE_ID]);
+
+    $novo_valor = $saldo - $data['valor'];
+    if($novo_valor < 0){
+        return ['ok' => false, "message" => 'Saldo insuficiente', 'code' => -1];
+    }
+
+    $cliente = $database->update("clientes", ["saldo" => $novo_valor], ["cliente_id" => CLIENTE_ID]);
+
+    if($database->errorInfo){
+        error_log(implode(', ', $database->errorInfo));
+        return ['ok' => false, "message" => $database->errorInfo[2], 'code' => $database->errorInfo[1]];
+    }
+
+    if(empty($cliente->rowCount())){
+        return ['ok' => false, "message" => 'Nenhuma alteração foi efetuada.', 'code' => -1];
+    }
+
+    return ['ok' => true, 'saldo' => $novo_valor];
+
+}
+
+//$rt = GetLanche(['lanche_id' => 2]);
+//print_r($rt);
+
+function GetLanche($dataf){
+
+    global  $database;
+
+    $validacoes = [
+        "lanche_id" => ["type" => "numeric", "required" => false],
+    ];
+
+    $validar = validarDados($validacoes, $dataf);
+
+    if($validar['ok'] === true){
+        $data = $validar['dados'];
+    }else{
+        return $validar;
+    }
+
+    $lanche_id = $data['lanche_id'];
+
+    $lanche = $database->get("lanche", "*", ['lanche_id' => $lanche_id]);
+
+    if($database->errorInfo){
+        error_log(implode(', ', $database->errorInfo));
+        return ['ok' => false, "message" => $database->errorInfo[2], 'code' => $database->errorInfo[1]];
+    }
+
+    if(empty($lanche)){
+        return ['ok' => false, "message" => 'Nenhuma dado foi retornado', 'code' => -1];
+    }
+
+    return ['ok' => true, 'lanche' => $lanche];
+
+}
+
+//$rt = GetLanches();
+//echo json_encode($rt);
+
+function GetLanches(){
+
+    global  $database;
+
+    $lanches = $database->select("lanche", "*");
+
+    if($database->errorInfo){
+        error_log(implode(', ', $database->errorInfo));
+        return ['ok' => false, "message" => $database->errorInfo[2], 'code' => $database->errorInfo[1]];
+    }
+
+    if(empty($lanches)){
+        return ['ok' => false, "message" => 'Nenhuma dado foi retornado', 'code' => -1];
+    }
+
+    return ['ok' => true, 'lanches' => $lanches];
+
+}
+
+//$rt = FazerPedido(['lanches' => [['lanche_id' =>  1, 'quantDispo' => 2], ['lanche_id' => 3, 'quantDispo' => 1]]]);
+//echo json_encode($rt);
+
+function FazerPedido($dataf){
+
+    global  $database;
+
+    $validacoes = [
+        "lanches" => ["type" => "list"]
+    ];
+
+    $validar = validarDados($validacoes, $dataf);
+
+    if($validar['ok'] === true){
+        $data = $validar['dados'];
+    }else{
+        return $validar;
+    }
+
+    $database->insert('pedidos', ['cliente_id' => 1]);
+    $pedido_id = $database->id();
+
+    $lanches = $data['lanches'];
+    foreach($lanches as $lanche){
+        $lanche['pedido_id'] = $pedido_id;
+        $database->insert('pedido_lanches', $lanche);
+    }
+
+    return ['ok' => true, 'pedido_id' => $pedido_id];
+
+}
+
+
+
+//$rt = DeleteLanche(['lanche_id' =>  3]);
+//echo json_encode($rt);
+
+function DeleteLanche($dataf){
+
+    global  $database;
+
+    $validacoes = [
+        "lanche_id" => ["type" => "numeric"],
+    ];
+
+    $validar = validarDados($validacoes, $dataf);
+
+    if($validar['ok'] === true){
+        $data = $validar['dados'];
+    }else{
+        return $validar;
+    }
+
+    $lanche_id = $data['lanche_id'];
+
+    $database->delete('lanche', ['lanche_id' => $lanche_id]);
+
+    return ['ok' => true, 'lanche_id' => $lanche_id];
+}
+
+//$rt = GetPedidos();
+//echo json_encode($rt);
+
+function GetPedidos(){
+
+    global  $database;
+
+    $pedidos_ids = $database->select("pedidos", "pedido_id");
+    $pedidos = [];
+    foreach($pedidos_ids as $pedido_id){
+        $lanches_ids = $database->select("pedido_lanches", "lanche_id", ['pedido_id' => $pedido_id]);
+        $pedido = [];
+        $lanches = [];
+        array_push($pedido, ['pedido_id' => $pedido_id]);
+        foreach($lanches_ids as $lanche_id){
+            $lanche = $database->get("lanche", "*", ['lanche_id' => $lanche_id]);
+            array_push($lanches, $lanche);
+        }
+        $pedidos['lanches'] = $lanches;
+    }
+
+    return ['ok' => true, 'pedidos' => $pedidos];
+
+}
+
+//$rt = CreateLanche(['nome' =>  'café', 'valor' => 2, 'descricao' => 'descricao', 'quantDispo' => 1]);
+//echo json_encode($rt);
+
+function CreateLanche($dataf){
+
+    global  $database;
+
+    $validacoes = [
+        "nome" => ["type" => "string"],
+        "valor" => ["type" => "numeric"],
+        "descricao" => ["type" => "string"],
+        "quantDispo" => ["type" => "numeric"],
+    ];
+
+    $validar = validarDados($validacoes, $dataf);
+
+    if($validar['ok'] === true){
+        $data = $validar['dados'];
+    }else{
+        return $validar;
+    }
+
+    $database->insert('lanche', $data);
+    $lanche_id = $database->id();
+
+    $validacoes = [
+        "foto" => ['id' => $lanche_id, 'type' => 'Imagem'],
+    ];
+
+    $fotosBD = [];
+
+    foreach($validacoes as $atrubuto => $argumentos){
+
+        $required = $argumentos['required'] ?? true;
+        unset($argumentos['required']);
+
+        if($required){
+            $arquivo = UploadFile($atrubuto, $argumentos);
+
+            if($arquivo['ok'] === false){
+                return $arquivo;
+            }
+
+            $fotosBD[$atrubuto] = $arquivo;
+        }
+    }
+
+    $database->update("lanche", $fotosBD, [
+        "lanche_id" => $lanche_id
+    ]);
+
+    return ['ok' => true, 'lanche_id' => $lanche_id];
+}
+
+function UploadFile($id){
+
+    if (!isset($_FILES['foto'])) {
+        return ['ok' => false, "message" => "imagem não recebida", 'code' => -1];
+    }
+
+    $imagem = $_FILES['foto'];
+
+    if (!($imagem["error"] == UPLOAD_ERR_OK)) {
+        return ['ok' => false, "message" => "Ocorreu um erro no upload do arquivo.", 'code' => -1];
+    }
+
+    $tamanho_maximo = 5 * 1024 * 1024;
+    if (!($imagem["size"] <= $tamanho_maximo)) {
+        return ['ok' => false, "message" => "O tamanho do arquivo excede o limite permitido.", 'code' => -1];
+    }
+
+    $fileInfo = pathinfo($imagem);
+    $extensao = $fileInfo['extension'];
+
+    $folderRaiz = "/arquivos";
+    $file_path = "$folderRaiz/$id.$extensao";
+
+
+   if (!move_uploaded_file($imagem["tmp_name"], $file_path)) {
+        return ['ok' => false, "message" => "Erro ao processar o arquivo", 'code' => -1];
+    }
+
+  return ['ok' => true, 'url' => "https://robertogram.com.br/terciodelivery/arquivos/$id.$extensao"];
+
+}
+
+function validarDados($validacoes, $dataf) {
+
+    $dadosValidados = [];
+
+    foreach ($validacoes as $campo => $configuracao) {
+        $valor = $dataf[$campo] ?? null;
+        $configuracao['required'] = $configuracao['required'] ?? true;
+
+        if ($configuracao['required'] &&  empty($valor) &&  $valor != 0) {
+            return ['ok' => false, 'message' => "campo -> $campo: obrigatório", 'code' => -1];
+        }
+
+        if (!isset($valor)) {
+            continue;
+        }
+
+        $tipo = $configuracao['type'];
+        if (($tipo === 'string' && !is_string($valor)) || ($tipo === 'numeric' && (!is_numeric($valor)) && $valor != 0) || ($tipo === 'list' && !array_is_list($valor)) || ($tipo === 'boolean' && !is_bool($valor)) || ($tipo === 'array' && !is_string($valor))) {
+            return ['ok' => false, 'message' => "campo -> $campo: tipo inválido / tipo correto '$tipo'", 'code' => -1];
+        }
+
+        if (isset($configuracao['sub_type'])) {
+            $sub_type = $configuracao['sub_type'];
+            foreach ($valor as $item) {
+                if (($sub_type === 'string' && !is_string($item)) || ($sub_type === 'numeric' && (!is_numeric($item)) && $item != 0) || ($sub_type === 'boolean' && !is_bool($item))) {
+                    return ['ok' => false, 'message' => "campo -> $campo: tipo inválido  / subtipo correto '$sub_type", 'code' => -1];
+                    break;
+                }
+            }
+        }
+
+        if (isset($configuracao['max_length']) && strlen($valor) > $configuracao['max_length']) {
+            return ['ok' => false, 'message' => "campo -> $campo: tamanho máximo excedido", 'code' => -1];
+        }
+
+        if (isset($configuracao['max_value']) && $valor > $configuracao['max_value']) {
+            return ['ok' => false, 'message' => "campo -> $campo: valor máximo excedido", 'code' => -1];
+        }
+
+        $dadosValidados[$campo] = $valor;
+    }
+
+    return ['ok' => true, 'dados' => $dadosValidados];
+}
+
+?>
